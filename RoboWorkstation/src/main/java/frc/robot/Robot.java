be4c42e4b518ea03483754099bdccd6fdd258398
Disguised Coffee <68920714+Disguised-Coffee@users.noreply.github.com>
2024-03-12 20:45:35 -0500

    Ensured Daily Save.

--- RoboWorkstation/src/main/java/frc/robot/Robot.java
+++ RoboWorkstation/src/main/java/frc/robot/Robot.java
@@ -4,7 +4,14 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.Faults;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,11 +24,27 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
-  @Override
-  public void robotInit() {}
+
+  TalonSRX m_motor = new TalonSRX(2);
+  Joystick r_joystick = new Joystick(0);
+  Faults _faults = new Faults();
+  // private int timeoutMs = 30;
 
   @Override
-  public void robotPeriodic() {}
+  public void robotInit() {
+    m_motor.configFactoryDefault();
+
+    m_motor.setInverted(true);
+
+    m_motor.setSensorPhase(false); // TODO
+  }
+
+  @Override
+  public void robotPeriodic() {
+    SmartDashboard.setDefaultNumber("Pos", m_motor.getSelectedSensorPosition());
+    SmartDashboard.setDefaultNumber("Velocity", m_motor.getSelectedSensorVelocity());
+    SmartDashboard.setDefaultBoolean("Out of Phase: ", _faults.SensorOutOfPhase);
+  }
 
   @Override
   public void autonomousInit() {}
@@ -30,13 +53,23 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {}
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    // TODO: experiment with these
+    // m_motor.configClearPositionOnLimitF(true, timeoutMs);
+    // m_motor.configClearPositionOnLimitR(true, timeoutMs);
+    // m_motor.configClearPositionOnQuadIdx(true, timeoutMs);
+    m_motor.setNeutralMode(NeutralMode.Brake);
+  }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    m_motor.set(TalonSRXControlMode.PercentOutput, r_joystick.getY());
+  }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    m_motor.setNeutralMode(NeutralMode.Coast);
+  }
 
   @Override
   public void disabledPeriodic() {}
